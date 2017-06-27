77bcfcd9341b6cbc90ca5df0bf1d8eeaa68730f8
Vitaliy Ostapenko <soul33d@gmail.com>
2017-06-28 00:33:01 +0300

    added ExecutorService, Semaphore, CountDownLatch demo

--- src/com/homelearning/FixedSizeQueueDemo.java
+++ src/com/homelearning/FixedSizeQueueDemo.java
@@ -5,25 +5,58 @@
 package com.homelearning;
 
 import java.util.Queue;
+import java.util.concurrent.*;
 
 public class FixedSizeQueueDemo {
     public static void main(String[] args) {
-        FixedSizeQueue<Integer> queue = new FixedSizeQueue<>(10);
+        int iterations = 1_000_000;
+        FixedSizeQueue<Integer> queue = new FixedSizeQueue<>(iterations);
         printQueue(queue);
         queue.add(999);
         queue.add(800000);
         printQueue(queue);
-        for (int i = 0; i < 16; i++) {
-            System.out.println("Adding " + i + " to queue.");
-            queue.add(i);
-            printQueue(queue);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Semaphore addSemaphore = new Semaphore(1);
+        Semaphore removeSemaphore = new Semaphore(0);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        executorService.execute(() -> {
+            for (int i = 0; i < iterations; i++) {
+                try {
+                    addSemaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                queue.add(i);
+                if (i%2 == 0) {
+                    addSemaphore.release();
+                    removeSemaphore.release();
+                }
+            }
+            countDownLatch.countDown();
+        });
+
+        executorService.execute(() -> {
+            for (int i = 0; i < iterations / 2; i++) {
+                try {
+                    removeSemaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                queue.poll();
+                addSemaphore.release();
+            }
+            countDownLatch.countDown();
+        });
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        int size = queue.size();
-        for (int i = 0; i < size; i++) {
-            System.out.print("Removing ");
-            System.out.println(queue.poll());
-            printQueue(queue);
-        }
+        printQueue(queue);
+        executorService.shutdown();
     }
 
     private static void printQueue(Queue<Integer> queue) {
